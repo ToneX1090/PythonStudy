010ba03139093a84a54eccce35571334146c67c6
ToneX1090 <nabor1210@gmail.com>
2022-03-08 21:18:28 -0600

    Brincadeira do Pi (incompleto)

---
+++ 013-jogo_pi.py
@@ -0,0 +1,10 @@
+numero = int (input ("Infome o até qual numero deseja brincar: "))
+contador = 0
+
+
+for i in range(numero):
+
+    if i % 7 = 0:
+        print ("PI")
+    else:
+        print (i+1)
