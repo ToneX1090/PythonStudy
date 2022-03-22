446116931dbe31dd52cbc8e7df43a403e6070901
ToneX1090 <nabor1210@gmail.com>
2022-03-21 20:56:13 -0600

    Jogo do PI completo

--- 013-jogo_pi.py
+++ 013-jogo_pi.py
@@ -1,15 +1,20 @@
-numero = int (input ("Infome o até qual numero deseja brincar: "))
+numero = int(input ("Infome o até qual numero deseja brincar: "))
 contador = 1
+contador1 = str(contador)
+
 
 while contador <= numero:
 
     if contador % 7 == 0:
 
         print("PI")
-    
-    
-    else:
 
+    elif ("7" in contador1):
+        print("PI")
+
+    else:
         print(contador)
+     
     
     contador = contador + 1
+    contador1 = str(contador)
