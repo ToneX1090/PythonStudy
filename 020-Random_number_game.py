68ea1c71f8548a1d2fc28d1217957d311d714354
ToneX1090 <nabor1210@gmail.com>
2022-07-04 22:22:40 -0600

    A game to generate a random number for the customer kick a number until hits

---
+++ 020-Random_number_game.py
@@ -0,0 +1,23 @@
+def game():
+
+    random_number = 10
+
+    kick_number = int(input("Informe o numero que deseja chutar: "))
+
+    while random_number != kick_number:
+
+        if random_number < kick_number:
+
+            new_kick= int (input ("Tente um numero mais baixo: "))
+
+        elif random_number > kick_number:
+            
+            new_kick= int (input ("Tente um numero mais alto: "))
+        
+        kick_number = new_kick
+
+    else:
+        print("Acertou 'Miseraví' !")
+
+
+game()
