789d3509b7cf93fd853cc880f6a2402f6e453fb2
ToneX1090 <nabor1210@gmail.com>
2022-07-12 21:57:11 -0600

    Recive a json by URL and print street, city, district and state

---
+++ 022-cep_get_link_Json.py
@@ -0,0 +1,15 @@
+import sys
+import requests
+
+cep = sys.argv[1]
+v_link = requests.get("https://viacep.com.br/ws/09091015/json/")
+v_json = v_link.json()
+
+if cep == v_json["cep"]:
+
+    print ("\nRua: " , v_json["logradouro"])
+    print ("Cidade: " , v_json["localidade"])
+    print ("Bairro: " , v_json["bairro"])
+    print ("Estado: " , v_json["uf"])
+
+    #tratar argumento .format .strip
