b83b06e81b280be23184e961aa001c802fb30642
ToneX1090 <nabor1210@gmail.com>
2022-07-18 22:33:08 -0600

    Search a postal code by Json and print the street, city, district and state.

--- 022-cep_get_link_Json.py
+++ 022-cep_get_link_Json.py
@@ -5,11 +5,7 @@ cep = sys.argv[1]
 v_link = requests.get("https://viacep.com.br/ws/"+cep+"/json/")
 v_json = v_link.json()
 
-if cep == v_json["cep"]:
-
-    print ("\nRua: " , v_json["logradouro"])
-    print ("Cidade: " , v_json["localidade"])
-    print ("Bairro: " , v_json["bairro"])
-    print ("Estado: " , v_json["uf"])
-
-    #tratar argumento .format .strip
+print ("\nRua: " , v_json["logradouro"])
+print ("Cidade: " , v_json["localidade"])
+print ("Bairro: " , v_json["bairro"])
+print ("Estado: " , v_json["uf"])
