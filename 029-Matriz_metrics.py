a46875d135066bec017a6867aceb612e475d6708
ToneX1090 <nabor1210@gmail.com>
2022-12-27 22:15:25 -0600

    Program 030 in progress

--- 029-Matriz_metrics.py
+++ 029-Matriz_metrics.py
@@ -1,25 +1,15 @@
-
-
-lists = [[3,5,7,9],[4,5,2,6],[1,4,5]]
-cont = 0
+lists = [[3,5,7,9],[4,5,2,6],[1,4,5,5]]
 list_number = []
 
 for list in lists:
 
     avg_list = sum(list) / len(list)
     
-    if list[cont] > avg_list:
-
-       del(list[cont])
-       cont = cont + 1
+    for element in list:
+        if element > avg_list:
+            list_number.append(element)
     
 
-    print(len(list), "numeros estão acima da média")
-    print(list)
-
-    #tentar com 2 for
-    #
-    ##
-    # for element in list:
-        #if element < avg_list:
-        #del(list[element])
+    print(len(list_number), "numeros estão acima da média",avg_list)
+    print(list_number)
+    list_number = []
---
+++ 030-Soccer_striker_average.py
@@ -0,0 +1,38 @@
+lists = [["Xulapa",3,0,1,1,2],["Dodo",2,1,1,2],["Didico",1,4,1,2],["Galinho",2,4,2,2]]
+dict = {}
+
+for list in range(len(lists)):
+    dict[lists[list][0]] = lists[list][1:]   
+  
+   
+
+
+print(dict)
+
+#     for striker in dict:
+#         striker = int(striker)
+
+#         sum_goals = sum(striker)
+
+    
+# print(sum_goals)
+
+
+
+
+
+
+# list_number = []
+
+# for list in lists:
+
+#     avg_list = sum(list) / len(list)
+    
+#     for element in list:
+#         if element > avg_list:
+#             list_number.append(element)
+    
+
+#     print(len(list_number), "numeros estão acima da média",avg_list)
+#     print(list_number)
+#     list_number = []
