c13c5c8a7e73fd2cee2013b88eb95c8701318ac6
ToneX1090 <nabor1210@gmail.com>
2023-01-18 22:38:22 -0600

    In progress

--- 031-Dolar_value_30sec.py
+++ 031-Dolar_value_30sec.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 import requests
+import time
 
+count = 20
 
-now = datetime.now()
-data = now.strftime("%d/%m/%Y %H:%M:%S")
 
 def search_dollar():
     database = requests.get("https://api.hgbrasil.com/finance?key=45550550")
@@ -11,5 +11,19 @@ def search_dollar():
 
     cotacao = json["results"]["currencies"]["USD"]
 
+   
+    print("O valor atual do dollar é : R$",cotacao["sell"])
+
+while count != 0:
+    
+    now = datetime.now()
+    data = now.strftime("%d/%m/%Y %H:%M:%S")
+    
     print(data)
-    print("A valor atual do dollar é : R$",cotacao["sell"])
+    search_dollar()
+    time.sleep(30)
+    count = count - 1
+
+
+#mostrar 2 numeros após a vorgula
+#melhorae vizualização da data
