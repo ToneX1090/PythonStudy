013c0a70db8ab42bf3415d78e7503d1fcb15ad28
ToneX1090 <nabor1210@gmail.com>
2023-02-21 20:53:54 -0600

    prints a rectangle of asterisks, the user informs the number of rows and columns

---
+++ 033_quadrado_asterisco.py
@@ -0,0 +1,29 @@
+print("Vamos fazer um quadrado de asteriscos(*)")
+colunas = int(input("digite o numero de colunas: "))
+linhas = int(input("digite o numero de linhas: "))
+ast = ""
+
+for i in range(colunas):
+
+    ast += "*"
+
+
+print(" ")
+for i in range(linhas):
+    
+    print(ast)
+
+
+
+
+
+
+
+
+#ast = []
+
+#for i in range(colunas):
+    #ast.append("*")
+
+#for i in range(linhas):
+    #print(ast)
