30bfb798d24a9df6d0ed99e4f1c9179a80410743
ToneX1090 <nabor1210@gmail.com>
2023-03-21 18:13:54 -0600

    in progress

--- 038_archive_info.py
+++ 038_archive_info.py
@@ -1,10 +1,15 @@
 import sys
 import os
+import os.path, time
 
-archive = open(sys.argv[1], "r", encoding='utf-8')
+archive = sys.argv[1]
 
-size = os.path.getsize(archive)
+tamanho_arquivo = os.stat(archive)
+data_arquivo = os.path.getctime(archive)
 
-print(size)
+print("O tamanho do arquivo é: " , tamanho_arquivo.st_size, "bytes")
+print("A ultima modificação do arquivo foi em: " , time.ctime(data_arquivo))
 
 # "C:\Users\Milton\Documents\chorris.txt"
+#archive = sys.argv[1]
+#archive = open(sys.argv[1], "r", encoding='utf-8')
