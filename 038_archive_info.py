49d5b1a315445c594aa1749df16a6d525c684c5f
ToneX1090 <nabor1210@gmail.com>
2023-04-02 19:25:56 -0600

    Creates a txt file containing the size, date of creation and modification of the file. The new file contains the suffix -info

--- 038_archive_info.py
+++ 038_archive_info.py
@@ -14,12 +14,14 @@ caminho = os.path.dirname(os.path.abspath(archive))
 
 tamanho_arquivo = os.stat(archive)
 data_arquivo = os.path.getmtime(archive)
-
+criacao_arquivo = os.path.getctime(archive)
 
 new_archive = open (f"{caminho}\{nome}-info.txt" , "w")
 
 new_archive.write(f"O tamanho do arquivo é: {tamanho_arquivo.st_size} bytes\n")
+new_archive.write(f"A criação do arquivo foi em: {time.ctime(criacao_arquivo)}\n")
 new_archive.write(f"A ultima modificação do arquivo foi em: {time.ctime(data_arquivo)}")
+
 
 new_archive.close()
 
