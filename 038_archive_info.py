bae87a4e8f3d809591d9aa78cef52f312a2195eb
ToneX1090 <nabor1210@gmail.com>
2023-04-02 19:14:52 -0600

    Creates a txt file with the same name as the chosen file with the suffix -info and creates a TXT containing the file information (size, date of creation and modification).

--- 038_archive_info.py
+++ 038_archive_info.py
@@ -4,20 +4,24 @@ import os.path, time
 
 archive = sys.argv[1]
 
+split = archive.split("\\")
+nome_txt = split[-1]
+nome_arquivo = nome_txt.split(".")
+nome = nome_arquivo[0]
+
+caminho = os.path.dirname(os.path.abspath(archive))
+
+
 tamanho_arquivo = os.stat(archive)
-data_arquivo = os.path.getctime(archive)
-
-#new_archive = open (f"{nome}-info.txt" , "a+")
-
-#new_archive.write("O tamanho do arquivo é: " , tamanho_arquivo.st_size, "bytes")
-#new_archive.write("A ultima modificação do arquivo foi em: " , time.ctime(data_arquivo))
-
-new_archive.close
-
-print("O tamanho do arquivo é: " , tamanho_arquivo.st_size, "bytes")
-print("A ultima modificação do arquivo foi em: " , time.ctime(data_arquivo))
+data_arquivo = os.path.getmtime(archive)
 
 
-# "C:\Users\Milton\Documents\chorris.txt"
-#archive = sys.argv[1]
-#archive = open(sys.argv[1], "r", encoding='utf-8')
+new_archive = open (f"{caminho}\{nome}-info.txt" , "w")
+
+new_archive.write(f"O tamanho do arquivo é: {tamanho_arquivo.st_size} bytes\n")
+new_archive.write(f"A ultima modificação do arquivo foi em: {time.ctime(data_arquivo)}")
+
+new_archive.close()
+
+
+# Teste > "C:\Users\Milton\Documents\chorris.txt"
