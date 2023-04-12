48c30e27504460e2864267a0f64bb2d6b5f981fe
ToneX1090 <nabor1210@gmail.com>
2023-04-11 20:11:26 -0600

    After the client types the name of a country, it consumes an API returning data from that same country.

--- 041_API_pais.py
+++ 041_API_pais.py
@@ -1,16 +1,20 @@
 import requests
 
 pais = input("Informe o país que deseja pesquisar: ")
+
 database = requests.get(f"https://restcountries.com/v3.1/name/{pais}?fullText=true")
 json = database.json()
 
-nome = json[0]["name"]
-continente = json[0]["continents"]
-linguas = json[0]["languages"]
+if type(json) == dict:
 
+    print(f"{pais} não foi encontrado, lembre-se que o nome deve estar em inglês. EX: Brazil")
 
+else:
+    nome = json[0]["name"]
+    continente = json[0]["continents"]
+    linguas = list(json[0]["languages"].values())
 
-print ("Nome: " , nome["common"])
-print ("Continente: " , continente[0])
-print ("População: " , json[0]["population"])
-print ("Linguas: " , linguas)
+    print ("Nome: " , nome["common"])
+    print ("Continente: " , continente[0])
+    print ("População: " , json[0]["population"])
+    print ("Linguas: " , ", ".join(linguas))
