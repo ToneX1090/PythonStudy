686f2fe38f94497a6f01a5479b2c471a1d246469
ToneX1090 <nabor1210@gmail.com>
2023-04-17 20:55:05 -0600

    In progress

---
+++ 042_Orientacao_objeto_OP.py
@@ -0,0 +1,23 @@
+class pirata(object):
+    pass
+
+luffy = pirata()
+luffy.nome = "Monkey D. Luffy"
+luffy.fruta = "Gomu Gomu"
+luffy.recompensa = "3.300.000.000"
+
+print(f"O pirata {luffy.nome} comeu a {luffy.fruta} e tem uma recompensa de {luffy.recompensa} de Berries.")
+
+law = pirata()
+law.nome = "Trafalgar D. Law"
+law.fruta = "Ope Ope"
+law.recompensa = "3.000.000.000"
+
+print(f"O pirata {law.nome} comeu a {law.fruta} e tem uma recompensa de {law.recompensa} de Berries.")
+
+shanks = pirata()
+shanks.nome = "Shanks O Ruivo"
+shanks.fruta = "No"
+shanks.recompensa = "4.048.900.000"
+
+print(f"O pirata {shanks.nome} comeu a {shanks.fruta} e tem uma recompensa de {shanks.recompensa} de Berries.")
