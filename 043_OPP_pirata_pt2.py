4123226c3e0ffe7e6615ce6a6bf538515a3f2320
ToneX1090 <nabor1210@gmail.com>
2023-05-12 20:17:54 -0600

    in progress

---
+++ 043_OPP_pirata_pt2.py
@@ -0,0 +1,19 @@
+class pirata(object):
+
+    def __init__(self, nome, recompensa, nome_fruta, raridade_fruta, nv_poder):
+        self.nome = nome
+        self.recompensa = float(recompensa)
+        self.nome_fruta = nome_fruta
+        self.raridade_fruta = raridade_fruta
+        self.poder = int(nv_poder)
+
+    #def battle_mode(self):
+|#colocar os supernovas
+luffy = pirata("Monkey D. Luffy", 3300000000,"Hito Hito", "rara", 4000)
+law = pirata("Trafalgar D. Law", 3000000000, "Ope Ope", "rara", 3000)
+shanks = pirata("Shanks O Ruivo", 4048900000, False, False, 4500)
+teach = pirata("Marchal D. Teach",3996000000, "Yami Yami", "Incomum", 4000)
+urogue = pirata("Monge Uroge", )
+    
+
+ 
