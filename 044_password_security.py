b33722b74c6a7f690cbe4c1548170b3ab6d2c385
ToneX1090 <nabor1210@gmail.com>
2024-01-31 20:37:49 -0600

    In progress

---
+++ 044_password_security.py
@@ -0,0 +1,13 @@
+password = input("Digite uma contrasenha forte que com mais de 8 caracteres: ")
+
+if len(password) < 8:
+       #print("A contrasenha não possui uma letra maiuscula, por favor, tente novamente!") 
+    print("A contrasenha informada tem menos de 8 caracteres, por favor, tente novamente!")
+else:
+    #verificar se contem maiuscula
+    #verificar se contem minuscula
+    #verificar se contem #%$%$#%
+    #verificar se contem numeros
+
+    else:
+        print("Nivel de contrasenha fraca!")
