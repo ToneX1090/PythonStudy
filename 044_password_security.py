95152d70a50d1f8ab75aa0d181caf4d963a8a137
ToneX1090 <nabor1210@gmail.com>
2024-02-05 21:40:57 -0600

    in progress

--- 044_password_security.py
+++ 044_password_security.py
@@ -4,10 +4,19 @@ if len(password) < 8:
        #print("A contrasenha não possui uma letra maiuscula, por favor, tente novamente!") 
     print("A contrasenha informada tem menos de 8 caracteres, por favor, tente novamente!")
 else:
+    '''
     #verificar se contem maiuscula
+    if not any(c.isupper() for c in password):
+    
     #verificar se contem minuscula
+    if not any(c.islower() for c in password):
+    
     #verificar se contem #%$%$#%
+    if not re.search(r'[#!@%&$]', password):
+    
     #verificar se contem numeros
+    if not any(c.isdigit() for c in password):
+    '''
 
     else:
         print("Nivel de contrasenha fraca!")
