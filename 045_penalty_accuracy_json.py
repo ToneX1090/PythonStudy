97b40c00a33d78e25168e531cec2d0190daaf2f7
ToneX1090 <nabor1210@gmail.com>
2024-02-07 21:27:54 -0600

    Penalty_accuracy updated.

--- 045_penalty_accuracy_json.py
+++ 045_penalty_accuracy_json.py
@@ -4,10 +4,19 @@ import json
 arquivo = open(sys.argv[1], "r", encoding='utf-8')
 json_txt = arquivo.read()
 
-real_json = json.loads(json_txt)
+jogadores = json.loads(json_txt)
+maior_taxa = {}
 
-print ("\nNome: " ,real_json["nome"])
-print ("penalties cobrados: " ,real_json["penalties_cobrados"])
+for jogador in jogadores:
+    jogador["taxa_de_conversão"] = (jogador["penalties_convertidos"] / jogador["penalties_cobrados"]) * 100
+
+    if len(maior_taxa) == 0:
+        maior_taxa = jogador
+    elif maior_taxa["taxa_de_conversão"] < jogador["taxa_de_conversão"]:
+        maior_taxa = jogador
+
+print("O jogador com maior taxa taxa de converção é: " ,maior_taxa["nome"])
+print(f'Com uma taxa de: {maior_taxa["taxa_de_conversão"]:.2f}%')
 
 
 #"C:\Users\Milton\Documents\penalty_json.txt"
