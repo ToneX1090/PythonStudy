67e997ddb4e2d81322149cff5b107b3cb6a8f6c4
ToneX1090 <nabor1210@gmail.com>
2024-02-22 20:32:51 -0600

    add the function that creates a new txt archive

--- 047_ISBN_new_archive_names.py
+++ 047_ISBN_new_archive_names.py
@@ -6,6 +6,17 @@ def title_name(ISBN):
     database = requests.get("https://openlibrary.org/isbn/"+ISBN+".json")
     json = database.json()
 
-    print("\nTitulo -" ,json["title"] ,json["subtitle"])
-
+    title = json["title"]
+    Subtitle = json["subtitle"]
+    
+    #print("\nTitulo -" ,json["title"] ,json["subtitle"])
 title_name()
+
+archive=open(sys.argv[1], "r")
+books = archive.readlines()
+
+newarchive = open("Booktitles.txt" , "w")
+    
+for book in books:
+    newarchive.write("\nTitulo -" ,json["title"] ,json["subtitle"]
+newarchive.close()
