26df2710b6e185f9b27e42d3c2927c7400cfa6dd
ToneX1090 <nabor1210@gmail.com>
2024-02-28 20:35:26 -0600

    trying resolve fixing the txt file, need to debug a little more

--- 047_ISBN_new_archive_names.py
+++ 047_ISBN_new_archive_names.py
@@ -6,21 +6,25 @@ def title_name(ISBN):
     database = requests.get("https://openlibrary.org/isbn/"+ISBN+".json")
     json = database.json()
 
+    title = json["title"]
+    print(title)
+    
+
     # full_title = json["full_title"]
     
     # return f"\nTitulo - {full_title}"
     
-    return json
+    # return json
 
 archive=open(sys.argv[1], "r")
 books = archive.readlines()
 
-newarchive = open("Booktitles.txt" , "w")
+# newarchive = open("Booktitles.txt" , "w")
     
 for book in books:
     #newarchive.write(title_name(book))
-    print(title_name(book))
-newarchive.close()
+    title_name(book)
+# newarchive.close()
 
 
-#"C:\Users\Milton\Documents\isbn_book.txt"
+#"C:\Users\Milton\Documents\047_livros.txt"
