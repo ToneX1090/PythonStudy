5be8c49098b322f3aa9aca2d67a25b21549aabb4
ToneX1090 <nabor1210@gmail.com>
2024-05-13 16:37:09 -0600

    Insert try/except validation

--- 047_ISBN_new_archive_names.py
+++ 047_ISBN_new_archive_names.py
@@ -2,29 +2,24 @@ import sys
 import requests
 
 def title_name(ISBN):
-
-    database = requests.get("https://openlibrary.org/isbn/"+ISBN+".json")
-    json = database.json()
-
-    title = json["title"]
-    print(title)
-    
-
-    # full_title = json["full_title"]
-    
-    # return f"\nTitulo - {full_title}"
-    
-    # return json
+    try:
+        database = requests.get("https://openlibrary.org/isbn/"+ISBN+".json")
+        json = database.json()
+    except:
+        newarchive.write("\n" +ISBN+"  : Este ISBN não é valido.")
+    else:
+        title = json["title"]
+        newarchive.write("\nTitulo: " +title)
 
 archive=open(sys.argv[1], "r")
 books = archive.readlines()
 
-# newarchive = open("Booktitles.txt" , "w")
+newarchive = open("Booktitles.txt" , "w")
     
 for book in books:
-    #newarchive.write(title_name(book))
     title_name(book)
-# newarchive.close()
+newarchive.close()
 
 
 #"C:\Users\Milton\Documents\047_livros.txt"
+#Necessário arrumar o try/except - inserir strip
