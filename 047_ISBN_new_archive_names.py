4df41214ec024e87f6f67a4e793e870a2a1c3491
ToneX1090 <nabor1210@gmail.com>
2024-05-20 18:30:07 -0600

    Get a json from the API and print the title of the books and an error mensage if the json doesn't exist

--- 047_ISBN_new_archive_names.py
+++ 047_ISBN_new_archive_names.py
@@ -2,14 +2,17 @@ import sys
 import requests
 
 def title_name(ISBN):
-    try:
-        database = requests.get("https://openlibrary.org/isbn/"+ISBN+".json")
-        json = database.json()
-    except:
-        newarchive.write("\n" +ISBN+"  : Este ISBN não é valido.")
+    response = requests.get("https://openlibrary.org/isbn/"+ISBN+".json")
+    if response.status_code != 200:
+        error_text = "\n" +ISBN.strip()+" : Este ISBN não é valido."
+        print(error_text)
+        newarchive.write(error_text)
     else:
+        json = response.json()
         title = json["title"]
-        newarchive.write("\nTitulo: " +title)
+        success_text = "\n" +ISBN.strip()+" : " +title
+        print(success_text)
+        newarchive.write(success_text)
 
 archive=open(sys.argv[1], "r")
 books = archive.readlines()
@@ -17,10 +20,8 @@ books = archive.readlines()
 newarchive = open("Booktitles.txt" , "w")
     
 for book in books:
-    realbook = book.strip()
-    title_name(realbook)
+    title_name(book)
 newarchive.close()
 
 
 #"C:\Users\Milton\Documents\047_livros.txt"
-#Necessário arrumar o try/except - inserir strip
