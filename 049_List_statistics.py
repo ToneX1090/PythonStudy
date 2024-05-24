0952d52880cd8f3fd2073396b67692363077570f
ToneX1090 <nabor1210@gmail.com>
2024-05-23 19:18:29 -0600

    Added the mode and median in the code.

--- 049_List_statistics.py
+++ 049_List_statistics.py
@@ -1,12 +1,13 @@
-list = [5,7,10,8,10,2,7,2,7]
+list = [5,7,8,10,2,7,2,6,10]
 sigma = 0
 
+#Average
 for number in list:
 
     sigma += number
 
 average = sigma/len(list)
-print(f"A média da lista é: {average}") #add decimals
+print(f"A média da lista é: {average:.2f}")
 
 cont = dict()
 
@@ -17,15 +18,25 @@ for number in list:
     else:
         cont[number] = 1
 
-#corrigir esta parte
+#Mode
 mode = 0
 value = 0 
-for k,v in sorted (cont.items()):
-               
-        if mode == 0:
-            mode = k
-        elif value < v:
-             mode = k
+for k,v in cont.items():
+    if v > value:
+        mode = k
         value = v
-        print(mode)
-print(cont)
+print(f"A moda é: {mode}")           
+
+#Median
+
+sorted_list = sorted(list)
+median = 0
+
+if len(list)%2 == 0:
+    left = sorted_list[len(list)//2]
+    right = sorted_list[(len(list)//2)-1]
+
+    median = (right+left)/2
+else:
+    median = sorted_list[len(list)//2]
+print(f"A mediana é: {median}")
