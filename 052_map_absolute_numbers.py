4e467364688556c8073be9f39de1f899c3da4503
ToneX1090 <nabor1210@gmail.com>
2024-06-06 13:57:32 -0600

    A code that receives a negative list numbers and return its absolute.

---
+++ 052_map_absolute_numbers.py
@@ -0,0 +1,8 @@
+negative_list = [-2, -4, -10, -5]
+
+absolute_numbers = map(abs, negative_list)
+
+absolute_list = list(absolute_numbers)
+
+print(f"A lista dada foi: {negative_list}")
+print(f"A lista absoluta é: {absolute_list}")
