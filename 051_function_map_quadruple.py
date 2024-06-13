925f102faf63cfb29fc9e20d765ef0fc80692f62
ToneX1090 <nabor1210@gmail.com>
2024-06-13 17:08:30 -0600

    Update the code to get a list by input.

--- 051_function_map_quadruple.py
+++ 051_function_map_quadruple.py
@@ -1,15 +1,14 @@
 def quadruple(x):
     return x * 4
 
-numbers = [1,2,3,4,10]
+numbers = (input("Insira a lista que deseja quadriplicar: "))
+real_numbers = numbers.split()
 
-# real_numbers = numbers****
-# int_numbers = list(map(int, real_numbers))
+int_numbers = map (int, real_numbers)
+int_list = list(int_numbers)
 
 
-result = map(quadruple, numbers)
+result = map(quadruple, int_list)
 quadruple_list = list(result)
 
 print(f"A lista quadruplicada é: {quadruple_list}")
-
-#Try interact a input
