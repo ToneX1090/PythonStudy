0d3b0403d0cda7c7b51c18f4edfe041792da4aff
ToneX1090 <nabor1210@gmail.com>
2024-06-19 18:20:03 -0600

    In progress

---
+++ 053_OPP_soccer.py
@@ -0,0 +1,29 @@
+class Player(object):
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Team(object):
+
+    def __init__(self,nome):
+        self.teamname = nome
+    
+    def add_player(self)
+        #develop
+    
+    def print_player(self)
+        print(f"O jogador {self.name} tem {self.age} anos.")
+
+
+Cassio = Player("Cassio", 34)
+Gamarra = Player("Gamarra", 53)
+Chicao = Player("Chicão", 43)
+Ze_Maria = Player("José Maria", 75)
+Roberto_Carlos
+Rincon
+Socrates
+Marcelinho
+Neto = Player("CRAQUE Neto", 57)
+Fenomeno = Player("Ronaldo", 47)
+Tevez
