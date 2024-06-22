d71e144aee33bc4c418def2a17f10d6d772475da
ToneX1090 <nabor1210@gmail.com>
2024-06-21 22:28:31 -0600

    Add the players and an empty player list

--- 053_OPP_soccer.py
+++ 053_OPP_soccer.py
@@ -6,13 +6,14 @@ class Player(object):
 
 class Team(object):
 
-    def __init__(self,nome):
+    def __init__(self,nome, players):
         self.teamname = nome
+        self.players = []
     
-    def add_player(self)
+    def add_player(self):
         #develop
     
-    def print_player(self)
+    def print_player(self):
         print(f"O jogador {self.name} tem {self.age} anos.")
 
 
@@ -20,10 +21,10 @@ Cassio = Player("Cassio", 34)
 Gamarra = Player("Gamarra", 53)
 Chicao = Player("Chicão", 43)
 Ze_Maria = Player("José Maria", 75)
-Roberto_Carlos
-Rincon
-Socrates
-Marcelinho
+Roberto_Carlos = Player("Roberto Carlos", 51)
+Rincon = Player("Freddy Rincon", 50)
+Socrates = Player("Sócrates", 58)
+Marcelinho = Player("Marcelo Pereira", 52)
 Neto = Player("CRAQUE Neto", 57)
 Fenomeno = Player("Ronaldo", 47)
-Tevez
+Tevez = Player("Carlitos Tevez", 40)
