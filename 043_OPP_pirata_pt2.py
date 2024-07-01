f4494c6f9f1b6ce6113ba2e0bd7ef5b0999ff318
ToneX1090 <nabor1210@gmail.com>
2024-07-01 17:00:54 -0600

    Add the pirates, fruits and data. Need to make de batle system

--- 043_OPP_pirata_pt2.py
+++ 043_OPP_pirata_pt2.py
@@ -1,26 +1,33 @@
-class pirata(object):
-
-    def __init__(self, nome, recompensa, nome_fruta, raridade_fruta, nv_poder):
+class Pirate(object):
+    def __init__(self, nome, recompensa, nome_fruta):
         self.nome = nome
         self.recompensa = float(recompensa)
         self.nome_fruta = nome_fruta
-        self.raridade_fruta = raridade_fruta
-        self.poder = int(nv_poder)
 
-    #def battle_mode(self):
-|#colocar os supernovas
-luffy = pirata("Monkey D. Luffy", 3300000000,"Hito Hito", "rara", 4000)
-law = pirata("Trafalgar D. Law", 3000000000, "Ope Ope", "rara", 3000)
-urogue = pirata("Monge Uroge", 108000000 )
-kid = pirata("Eustass Kid", 3000000000,)
-hawkins = pirata("Basil Hawkins", 320000000,)
-drake = pirata("X Drake", 222000000,)
-apoo = pirata("Scratchmen Apoo", 350000000)
-killer = pirata("Killer", 162000000,)
-bonney = pirata("Jewelry Bonney", 140000000)
-bege = pirata("Capone Gang Bege", 138000000)
-zoro  = pirata("Roronoa Zoro", 1111000000)
+class Frute(object):
+    def __init__(self,common,uncommon,rare,power_lv):
+        self.common = common
+        self.uncommon = uncommon
+        self.rare = rare
+        self.power_lv = int(power_lv)
 
-    
+class Battle(object):
+    def __init__(self):
+
+
+#colocar os supernovas
+luffy = Pirate("Monkey D. Luffy", 3300000000,"Hito Hito", "rare", 4000)
+law = Pirate("Trafalgar D. Law", 3000000000, "Ope Ope", "rare", 3000)
+urogue = Pirate("Monge Uroge", 108000000, "Abe Abe", "uncommon", 2500)
+kid = Pirate("Eustass Kid", 3000000000,"Jiki Jiki", "uncommon", 3000)
+hawkins = Pirate("Basil Hawkins", 320000000, "Wara Wara", "Uncommon", 2000)
+drake = Pirate("X Drake", 222000000, "Ryu Ryu", "rare", 2000)
+apoo = Pirate("Scratchmen Apoo", 350000000, "Oto Oto", "common",2000 )
+killer = Pirate("Killer", 162000000, "smile", "common", 2500)
+bonney = Pirate("Jewelry Bonney", 140000000, "Toshi Toshi", "rare", 2000)
+bege = Pirate("Capone Gang Bege", 138000000, "Shiro Shiro", "common", 2000)
+zoro  = Pirate("Roronoa Zoro", 1111000000, False, False, 2600)
+
+
 
  
