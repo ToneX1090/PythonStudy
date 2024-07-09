e09afb0adce9bde79f907bf7712b7d4963040ce7
ToneX1090 <nabor1210@gmail.com>
2024-07-09 16:34:35 -0600

    A pirate battle using 2 builders and a battle module copares 2 pirates(based in One Piece)

--- 043_OPP_pirata_pt2.py
+++ 043_OPP_pirata_pt2.py
@@ -1,33 +1,58 @@
 class Pirate(object):
-    def __init__(self, nome, recompensa, nome_fruta):
-        self.nome = nome
-        self.recompensa = float(recompensa)
-        self.nome_fruta = nome_fruta
+    def __init__(self, name, reward, fruit):
+        self.name = name
+        self.reward = float(reward)
+        self.fruit = fruit
 
-class Frute(object):
-    def __init__(self,common,uncommon,rare,power_lv):
-        self.common = common
-        self.uncommon = uncommon
-        self.rare = rare
+class Fruit(object):
+    def __init__(self,fruit_name, rarity, power_lv):
+        self.fruit_name = fruit_name 
+        self.rarity = rarity
         self.power_lv = int(power_lv)
 
 class Battle(object):
-    def __init__(self):
+    def battle (self, pirate1, pirate2):
+
+        pirate1_power = pirate1.reward
+        
+        if pirate1.fruit != None:
+            pirate1_power += pirate1.fruit.power_lv
+        
+        pirate2_power = pirate2.reward 
+
+        if pirate2.fruit != None:
+            pirate2_power += pirate2.fruit.power_lv
+
+        if pirate1_power > pirate2_power:
+            print(f"O pirata {pirate1.name} ganhou a batalha")
+        else:
+            print(f"O pirata {pirate2.name} ganhou a batalha")
 
 
-#colocar os supernovas
-luffy = Pirate("Monkey D. Luffy", 3300000000,"Hito Hito", "rare", 4000)
-law = Pirate("Trafalgar D. Law", 3000000000, "Ope Ope", "rare", 3000)
-urogue = Pirate("Monge Uroge", 108000000, "Abe Abe", "uncommon", 2500)
-kid = Pirate("Eustass Kid", 3000000000,"Jiki Jiki", "uncommon", 3000)
-hawkins = Pirate("Basil Hawkins", 320000000, "Wara Wara", "Uncommon", 2000)
-drake = Pirate("X Drake", 222000000, "Ryu Ryu", "rare", 2000)
-apoo = Pirate("Scratchmen Apoo", 350000000, "Oto Oto", "common",2000 )
-killer = Pirate("Killer", 162000000, "smile", "common", 2500)
-bonney = Pirate("Jewelry Bonney", 140000000, "Toshi Toshi", "rare", 2000)
-bege = Pirate("Capone Gang Bege", 138000000, "Shiro Shiro", "common", 2000)
-zoro  = Pirate("Roronoa Zoro", 1111000000, False, False, 2600)
+#fruits
+fruit_1 = Fruit("Hito Hito", "rare", 4000)
+fruit_2 = Fruit("Ope Ope", "rare", 3000)
+fruit_3 = Fruit("Abe Abe", "uncommon", 2500)
+fruit_4 = Fruit("Jiki Jiki", "uncommon", 3000)
+fruit_5 = Fruit("Wara Wara", "uncommon", 2000)
+fruit_6 = Fruit("Ryu Ryu", "rare", 2000)
+fruit_7 = Fruit("Oto Oto", "common", 2000)
+fruit_8 = Fruit("Smile", "common", 2500)
+fruit_9 = Fruit("Toshi Toshi", "rare", 2000)
+fruit_10 = Fruit("Shiro Shiro", "common", 2000)
 
+#pirates
+luffy = Pirate("Monkey D. Luffy", 3300000000, fruit_1)
+law = Pirate("Trafalgar D. Law", 3000000000, fruit_2)
+urogue = Pirate("Monge Uroge", 108000000, fruit_3)
+kid = Pirate("Eustass Kid", 3000000000,fruit_4)
+hawkins = Pirate("Basil Hawkins", 320000000, fruit_5)
+drake = Pirate("X Drake", 222000000, fruit_6)
+apoo = Pirate("Scratchmen Apoo", 350000000, fruit_7)
+killer = Pirate("Killer", 162000000, fruit_8)
+bonney = Pirate("Jewelry Bonney", 140000000, fruit_9)
+bege = Pirate("Capone Gang Bege", 138000000, fruit_10)
+zoro  = Pirate("Roronoa Zoro", 1111000000, None)
 
-
- 
+Battle().battle(luffy,law)
+Battle().battle(zoro,killer)
