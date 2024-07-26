18babd950330f03ed301c712aced35d859176089
ToneX1090 <nabor1210@gmail.com>
2024-07-26 17:42:53 -0600

    add coments about the logic

--- 054_brazilian_soccer_championship.py
+++ 054_brazilian_soccer_championship.py
@@ -7,40 +7,55 @@ class Team(object):
 class Championship(object):
     def __init__(self):
         self.table = []
-    
 
-    def first_round(self, team):
+    def add_team(self, team):
         self.table.append(team)
 
-Team("Athletico", 6, 0)
-Team("Atlético-GO",4, 0)
-Team("Atlético-MG",8, 0)
-Team("Bahia",6, 0)
-Team("Botafogo",6 ,0)
-Team("Bragantino",7, 0)
-Team("Corinthians", 5, 0)
-Team("Criciúma", 5, 0)
-Team("Cruzeiro", 7, 0)
-Team("Cuiabá", 4, 0)
-Team("Flamengo", 8, 0)
-Team("Fluminense", 3, 0)
-Team("Fortaleza", 6, 0)
-Team("Grêmio", 3, 0)
-Team("Internacional", 4, 0)
-Team("Juventude", 2, 0)
-Team("Palmeiras", 8, 0)
-Team("São Paulo", 6, 0)
-Team("Vasco", 3, 0)
-Team("Vitória", 3, 0)
+    def first_round()
+        #criar um laço que mantém fixo o primeiro time e o compara com todos os outros
+
+        # Simular todos os jogos
+            # for i in range(len(self.table)):
+            #     for j in range(i + 1, len(self.table)):
+            #         team1 = teams[i]
+            #         team2 = teams[j]
 
 
 
+        #somar o numero de pontos feitos em uma variavel points
+
+        #if team1.team_lv == team2.team_lv:
+        #   team1.points += 1
+        #elif team1.team_lv > team2.team_lv:        
+        #    team1.points += 3
+        #else:
+        #    team1.points += 0
+        #         
+        #repetir para todos os times
 
 
+brazilian_championship = Championship()  
 
-
-
-
+brazilian_championship.add_team(Team("Athletico-PA", 6, 0))
+brazilian_championship.add_team(Team("Atlético-GO",4, 0))
+brazilian_championship.add_team(Team("Atlético-MG",8, 0))
+brazilian_championship.add_team(Team("Bahia",6, 0))
+brazilian_championship.add_team(Team("Botafogo",6 ,0))
+brazilian_championship.add_team(Team("Bragantino",7, 0))
+brazilian_championship.add_team(Team("Corinthians", 5, 0))
+brazilian_championship.add_team(Team("Criciúma", 5, 0))
+brazilian_championship.add_team(Team("Cruzeiro", 7, 0))
+brazilian_championship.add_team(Team("Cuiabá", 4, 0))
+brazilian_championship.add_team(Team("Flamengo", 8, 0))
+brazilian_championship.add_team(Team("Fluminense", 3, 0))
+brazilian_championship.add_team(Team("Fortaleza", 6, 0))
+brazilian_championship.add_team(Team("Grêmio", 3, 0))
+brazilian_championship.add_team(Team("Internacional", 4, 0))
+brazilian_championship.add_team(Team("Juventude", 2, 0))
+brazilian_championship.add_team(Team("Palmeiras", 8, 0))
+brazilian_championship.add_team(Team("São Paulo", 6, 0))
+brazilian_championship.add_team(Team("Vasco", 3, 0))
+brazilian_championship.add_team(Team("Vitória", 3, 0))
 
 #campeonato
 #simular 38 rodadas 
